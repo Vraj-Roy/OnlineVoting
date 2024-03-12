abd3127c4a801a3c8ec1a31e6cc408a66ac12d6c
Vraj-11 <vrajroy2k19@gmail.com>
2024-03-12 12:53:31 +0530

    fix5.3

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Navbar from "@/components/navbar";
 import "@/styles/globals.css";
+import Head from "next/head";
 import { useState } from "react";
 import LoadingBar from "react-top-loading-bar";
 
@@ -11,29 +12,31 @@ export default function App({ Component, pageProps }) {
   };
   return (
     <>
-      <meta property="og:title" content="Online Voting System" />
-      <meta
-        property="og:description"
-        content="Vote Online Today
+      <Head>
+        <meta property="og:title" content="Online Voting System" />
+        <meta
+          property="og:description"
+          content="Vote Online Today
 "
-      />
-      <meta property="og:image" content="/main.png" />
-      <meta
-        property="og:url"
-        content="https://online-voting-systemm.vercel.app/"
-      />
-      <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="Online Voting System" />
-      <LoadingBar
-        color="#3B82F6"
-        progress={progress}
-        height={4}
-        transitionTime={400}
-        loaderSpeed={300}
-        shadow={true}
-        waitingTime={400}
-        onLoaderFinished={() => setProgress(0)}
-      />
+        />
+        <meta property="og:image" content="/main.png" />
+        <meta
+          property="og:url"
+          content="https://online-voting-systemm.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Online Voting System" />
+        <LoadingBar
+          color="#3B82F6"
+          progress={progress}
+          height={4}
+          transitionTime={400}
+          loaderSpeed={300}
+          shadow={true}
+          waitingTime={400}
+          onLoaderFinished={() => setProgress(0)}
+        />
+      </Head>
       <Navbar resetKey={resetKey} key={key} setProgress={setProgress} />
       <Component {...pageProps} resetKey={resetKey} setProgress={setProgress} />
     </>
