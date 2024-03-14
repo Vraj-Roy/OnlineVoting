1bde71013a27057e48b7ab6ff990b6a15958bdfc
Vraj-11 <vrajroy2k19@gmail.com>
2024-03-14 09:16:06 +0530

    staticProps

--- pages/ongoing/index.js
+++ pages/ongoing/index.js
@@ -10,7 +10,7 @@ const Ongoing = ({ data }) => {
   return (
     <>
       <div className="md:w-[60%] w-[90%] m-auto mt-10  rounded-md p-10 shadow-xl shadow-grey-500  font-bold text-center my-2 ">
-        <div className="text-xl md:text-4xl">Ongoing Elections</div>
+        <div className="text-xl md:text-4xl">Ongoing Elections </div>
         <div className=" block md:flex   relative  flex-nowrap md:flex-wrap  mx-auto w-full  justify-between  m-auto">
           {data &&
             data.E.map((ee) => {
