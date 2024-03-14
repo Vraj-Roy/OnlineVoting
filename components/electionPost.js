df0a493e339ba7daabd34533e1942bf94e4220f3
Vraj-11 <vrajroy2k19@gmail.com>
2024-03-14 09:12:11 +0530

    staticProps

--- components/electionPost.js
+++ components/electionPost.js
@@ -21,7 +21,7 @@ const ElectionPost = ({ e_name, logo, totalCandidates }) => {
 
       <div className="lg:w-[45%]">
         <div
-          className="grid grid-cols-2 w-full mt-10 p-5 shadow-lg shadow-blue-600  rounded-xl cursor-pointer "
+          className="grid grid-cols-2 w-full mt-10 p-5 shadow-lg shadow-gray-500  rounded-xl cursor-pointer "
           onClick={() => Router.push(`./ongoing/${e_name}`)}
         >
           <div className="w-20 h-20  border rounded-full overflow-hidden ">
--- pages/ongoing/index.js
+++ pages/ongoing/index.js
@@ -1,37 +1,18 @@
-import Navbar from "@/components/navbar";
 import ElectionPost from "@/components/electionPost";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-const Ongoing = () => {
-  const router = useRouter();
-  const [data, setData] = useState("");
-  const [loader, setLoader] = useState(false);
-  useEffect(() => {
-    if (!localStorage.getItem("token")) {
-      router.push("./login");
-    } else {
-    }
-    loadElections();
-  }, []);
-  const loadElections = async () => {
-    let res = await fetch(`/api/election`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    let response = await res.json();
-    console.log(response);
-    setData(response);
-    setLoader(true);
-  };
 
+export async function getStaticProps() {
+  const res = await fetch(process.env.URL + "/api/election");
+  const data = await res.json();
+  return { props: { data } };
+}
+
+const Ongoing = ({ data }) => {
   return (
     <>
       <div className="md:w-[60%] w-[90%] m-auto mt-10  rounded-md p-10 shadow-xl shadow-grey-500  font-bold text-center my-2 ">
         <div className="text-xl md:text-4xl">Ongoing Elections</div>
         <div className=" block md:flex   relative  flex-nowrap md:flex-wrap  mx-auto w-full  justify-between  m-auto">
-          {loader &&
+          {data &&
             data.E.map((ee) => {
               return (
                 <ElectionPost
