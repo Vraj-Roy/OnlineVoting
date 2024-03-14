ad4192b54148a202b7326df9b71a479d1b9e304d
Vraj-11 <vrajroy2k19@gmail.com>
2024-03-14 09:27:51 +0530

    staticProps-fix3

--- pages/api/getCandidates.js
+++ pages/api/getCandidates.js
@@ -4,7 +4,9 @@ const jwt = require("jsonwebtoken");
 
 const getCandidates = async (req, res) => {
   await connectDb();
-
+  if (!req.body.token) {
+    return res.json({ success: false });
+  }
   let u = jwt.verify(req.body.token, "!@#DhoniMahiThala#@!");
   let E = await Election.findOne({ election_name: req.body.slug });
   let voters = [];
