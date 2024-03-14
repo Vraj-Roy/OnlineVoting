535f33a793d9e9d9a2430948b8bc2efd684395ca
Vraj-11 <vrajroy2k19@gmail.com>
2024-03-14 09:21:30 +0530

    staticProps-fix1

--- pages/ongoing/index.js
+++ pages/ongoing/index.js
@@ -1,7 +1,9 @@
 import ElectionPost from "@/components/electionPost";
 
 export async function getStaticProps() {
-  const res = await fetch("online-voting-systemm.vercel.app" + "/api/election");
+  const res = await fetch(
+    "https://online-voting-systemm.vercel.app/" + "/api/election"
+  );
   const data = await res.json();
   return { props: { data } };
 }
